4950c069af9e33760d4656eaa16594b08f428730
Canary80 <alexanderjamesmeakin@hotmail.com>
2017-02-09 22:22:12 +0000

    Updated multiple operand operation

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ function runningSum(last_number) {
   var last_value = Number(last_number);
   switch (lastoperator) {
   case 'x':
-    console.log('Got here');
     runtotal = runtotal * last_value;
+    console.log(runtotal);
     break;  
   case '+':
     runtotal = runtotal + last_value;
@@ -15,11 +15,10 @@ function runningSum(last_number) {
     runtotal = runtotal - last_value;
     break;
   case '/':
-    runtotal = runtotal - last_value;
+    runtotal = runtotal / last_value;
     break;
   default:
   	runtotal = last_value;
-  	console.log(runtotal);
   	break;
   }
   return;
@@ -28,16 +27,23 @@ function runningSum(last_number) {
 
 $(function () {
   $('#equals').on('click', function(e) {
-    var last_number = $('#runsum').text().split(/\D/g).slice(-2,-1)[0];
-    runningSum(last_number);
-    $('#total').text(runtotal.toString()); 
+    if ( $('#runsum').text().split(/\D/g).slice(-1)[0] == "") {
+      $('#total').text(runtotal.toString());
+    }
+    else {
+      var last_number = $('#runsum').text().split(/\D/g).slice(-1)[0];
+      console.log(last_number);
+      runningSum(last_number);
+      $('#total').text(runtotal.toString()); 
+    }
     $('#runsum').text(''); 
     lastoperator='';
   });
   
   $('.operand').on('click', function(e) {
     $('#runsum').append(e.target.textContent);
-    var last_number = $('#runsum').text().split(/\D/g).slice(-2,-1)[0];
+    var last_number = $('#runsum').text().split(/\D+/g).filter(function(n){ return n != "" })[0];
+    //console.log(last_number);
     runningSum(last_number);
     lastoperator = $('#runsum').text().slice(-1); 
     console.log(lastoperator);
@@ -45,5 +51,13 @@ $(function () {
   
 	$('.buttons').not('.action').on('click', function(e) {
   	$('#runsum').append(e.target.textContent); 
+  });
+
+  $('#clear').on('click', function(e) {
+    $('#runsum').text('');
+    $('#total').text('0'); 
+    runtotal = 0;
+    lastoperator = '';
+
   });
 });
